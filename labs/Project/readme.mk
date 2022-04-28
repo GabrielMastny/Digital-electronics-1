1b8e1f33e6429db660509c5b068389b2f7f390b1
Gabriel Mastný <mastny.gabriel@gmail.com>
2022-04-28 19:47:54 +0200

    Update readme.mk

--- labs/Project/readme.mk
+++ labs/Project/readme.mk
@@ -1,1 +1,51 @@
+# YOUR_PROJECT_TITLE
 
+### Team members
+
+* Mastný - všechno
+* Fedor - všechno
+
+### Table of contents
+
+* [Project objectives](#objectives)
+* [Hardware description](#hardware)
+* [VHDL modules description and simulations](#modules)
+* [TOP module description and simulations](#top)
+* [Video](#video)
+* [References](#references)
+
+<a name="objectives"></a>
+
+## Project objectives
+
+Write your text here.
+
+<a name="hardware"></a>
+
+## Hardware description
+
+Write your text here.
+
+<a name="modules"></a>
+
+## VHDL modules description and simulations
+
+Write your text here.
+
+<a name="top"></a>
+
+## TOP module description and simulations
+
+Write your text here.
+
+<a name="video"></a>
+
+## Video
+
+Write your text here
+
+<a name="references"></a>
+
+## References
+
+1. Write your text here.
